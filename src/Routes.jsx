cd4b596ce03fa34ce0e8b29f989d4b71afb5f33c
Ankit Verma <syncing284@gmail.com>
2025-12-03 12:10:00 +0530

    make changes and resolve errors

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter, Routes as RouterRoutes, Route } from "react-router-dom";
-import ScrollToTop from "components/ScrollToTop";
-import ErrorBoundary from "components/ErrorBoundary";
-import NotFound from "pages/NotFound";
+import ScrollToTop from "./components/ScrollToTop";
+import ErrorBoundary from "./components/ErrorBoundary";
+import NotFound from "./pages/NotFound";
 import ChatLogViewer from './pages/chat-log-viewer';
 import CustomerChatInterface from './pages/customer-chat-interface';
 import AdminDashboard from './pages/admin-dashboard';
